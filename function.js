98110a779a979e60d76e17855730981983f5cedf
bennz03 <bennyhinm18@gmail.com>
2025-01-13 12:34:20 +0530

    DAY 5 TASK

---
+++ function.js
@@ -0,0 +1,24 @@
+function SECE(dept){
+    return "Section of the department is A";
+}
+console.log(SECE("cse"));
+
+function SECE2025(dept){
+    console.log("The dept is : ",dept);
+}
+SECE("cse");
+
+function Add(){
+    return "Addion";
+}
+console.log(Add());
+
+function sub(){
+    console.log("Subtraction");
+
+}
+sub();
+
+
+
+
