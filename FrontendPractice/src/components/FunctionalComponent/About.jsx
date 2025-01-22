439d8e8941d32622b207a988fef2974e04be0339
bennz03 <bennyhinm18@gmail.com>
2025-01-22 14:41:41 +0530

    DAY 8 TASK

---
+++ FrontendPractice/src/components/FunctionalComponent/About.jsx
@@ -0,0 +1,20 @@
+import {useState} from 'react'
+const About=()=>{
+    var[text,setText]=useState("Vijay")
+    function handledText(e)
+    {
+        setText(e.target.value)
+    }
+    return(
+        <div>
+
+            <h1> this is About Component.</h1>
+            <h2>TextArea.</h2>
+            <textarea value={text} onChange={handledText}
+            
+            />
+            <p>The text is {text}</p>
+        </div>
+    )
+}
+export default About;
