5413135f6cd10fd84206b1c1bada2861c73f190d
bennz03 <bennyhinm18@gmail.com>
2025-01-27 21:10:43 +0530

    DAY 13 TASK

---
+++ seceBackend/models/signupSchema.js
@@ -0,0 +1,11 @@
+const mdb = require('mongoose');
+const signupSchema = mdb.Schema({
+    firstName: Ben,
+    lastName: Hin,
+    userName: Ben03,
+    email: Bennyhinm18,
+    password: Hin03,
+})
+
+const signup_schema = mdb.model("signup", signupSchema);
+module.exports = signup_schema;
